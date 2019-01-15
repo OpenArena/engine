db64ddcc2c62078b910803a459e702e9034b12b8
Henry Poggie <henry.poggie@gmail.com>
2019-01-14 19:45:49 -0500

    Fix strncpy copying a string onto itself

--- code/client/cl_cgame.c
+++ code/client/cl_cgame.c
@@ -618,7 +618,9 @@ intptr_t CL_CgameSystemCalls( intptr_t *args ) {
 		Com_Memcpy( VMA(1), VMA(2), args[3] );
 		return 0;
 	case CG_STRNCPY:
-		strncpy( VMA(1), VMA(2), args[3] );
+		if (VMA(1) != VMA(2)) {
+			strncpy( VMA(1), VMA(2), args[3] );
+		}
 		return args[1];
 	case CG_SIN:
 		return FloatAsInt( sin( VMF(1) ) );
